75104453dd6fa073fe1d9e98aec7639697688e20
Matthew Statongit config --global user.name Matthew <Malanstaton@gmail.com>
2019-09-09 22:17:12 -0400

    setup

---
+++ assets/JS/app.js
@@ -0,0 +1,21 @@
+// Set Variables 
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+
+
+ctx.beginPath();
+ctx.rect(10, 10, 20, 20);
+ctx.fillStyle = "green";
+ctx.fill();
+ctx.closePath();
+
+
+
+
+
+
+
+
+
+
+
